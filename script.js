b5e758960867307acb12db6f4ada87355af8e2f0
msalam <ms17alam@gmail.com>
2018-03-01 13:52:46 -0500

    some funck

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // Wining score
-var heightJohn = 220;
-var heightMike = 124;
-var ageJohn = 30;
-var ageMike = 31;
-
-var scoreJohn = heightJohn + 5 * ageJohn;
-var scoreMike = heightMike + 5 * ageMike;
-
-if (scoreJohn > scoreMike){
-  alert("John is the winner and his score is: " + scoreJohn)
-}else if(scoreJohn < scoreMike){
-  alert("Mike is the winner and his score is: " + scoreMike)
-}else{
-  alert("Bothe of your score in equal")
-}
+// var heightJohn = 220;
+// var heightMike = 124;
+// var ageJohn = 30;
+// var ageMike = 31;
+//
+// var scoreJohn = heightJohn + 5 * ageJohn;
+// var scoreMike = heightMike + 5 * ageMike;
+//
+// if (scoreJohn > scoreMike){
+//   alert("John is the winner and his score is: " + scoreJohn)
+// }else if(scoreJohn < scoreMike){
+//   alert("Mike is the winner and his score is: " + scoreMike)
+// }else{
+//   alert("Bothe of your score in equal")
+// }
 
 // Using function in another Function
 
@@ -100,3 +100,24 @@ function first(){
     console.log(a + b + c);
   }
 }
+
+// Method in a Object can be borrowed to use in another object
+
+var brian = {
+  name: "Brian",
+  yearOfBirth: 1940,
+  calculateAge: function(year){
+    console.log(2018 - this.yearOfBirth);
+  }
+} ;
+
+brian.calculateAge();
+
+var mike = {
+  name: "Mike",
+  yearOfBirth: 1945
+};
+
+mike.calculateAge = brian.calculateAge;
+
+mike.calculateAge();
